1054c7b99648f4ec52e0bac34fb7ddebfac36d65
Hossein <fazel.hosein9654@gmail.com>
2024-07-30 15:54:51 +0330

    add time and energy left in log_system print function ___var_4

--- Log.cpp
+++ Log.cpp
@@ -18,9 +18,11 @@ void log_system::print()
 {
     for(const auto item : this->logs)
     {
-        cout << "Current Location : " << item.__location._X << "  " << item.__location._Y << endl;
+        cout << "Current Location :  " << "x = " << item.__location._X << ",  y = " << item.__location._Y << endl;
+        cout << "Energy left      :  " << item._energy_left << endl;
+        cout << "Time left        :  " << item._time_left << endl;
         // cout << "What i see ?"
-        cout << "The next move : " << item._next_move << endl;
+        cout << "The next move    :  " << item._next_move << endl;
         cout << "\t\t********************************** \n\n";
     }
 }
