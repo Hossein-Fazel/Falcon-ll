0a3c40449d6fab3584d16ad0160961c2a46526c5
Hossein <fazel.hosein9654@gmail.com>
2024-07-30 11:20:53 +0330

    add class log_system ___var_3

--- Log.hpp
+++ Log.hpp
@@ -1,8 +1,9 @@
-#include <iostream>
-#include "DataStruct__Func.hpp"
-
 #ifndef LOG_HPP
 #define LOG_HPP
+#include <iostream>
+#include <vector>
+#include "DataStruct__Func.hpp"
+using namespace std;
 
 struct SaveThings
 {
@@ -10,4 +11,17 @@ struct SaveThings
     std::string _next_move;
     //What I See? variable
 };
+
+class log_system
+{
+    public:
+        log_system();
+        void add(Location loc, string move);
+        void print();
+
+    private:
+        vector<SaveThings> logs;
+
+};
+
 #endif
