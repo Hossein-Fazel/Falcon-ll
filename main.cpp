4468b20cc61a36883331175754455dc47dc2c0f0
AAzizkhani <aliazizkhani90@gmail.com>
2024-07-31 18:46:45 +0330

    if statement of visit ____ver_8

--- main.cpp
+++ main.cpp
@@ -92,4 +92,45 @@ int main()
             space_map.set_data(i, j, cell_data);
         }
     }
+
+    do
+    {
+        vector<int> data;
+
+        int y = space_ship._get_Location()._Y;
+        int x = space_ship._get_Location()._X;
+
+        space_map.set_info(x ,y , visited_map);
+
+        if(visited_map[x][y] != 0 and visited_map[x][y] != 2)
+            data.push_back(visited_map[x][y]);   
+
+        if(x + 1 < row and visited_map[x+1][y] != 0 and visited_map[x+1][y] != 2 )
+            data.push_back(visited_map[x+1][y]); 
+
+        if(x - 1 > 0 and visited_map[x+1][y] != 0 and visited_map[x+1][y] != 2)
+            data.push_back(visited_map[x-1][y]);
+        
+        if(y + 1 < column and visited_map[x+1][y] != 0 and visited_map[x+1][y] != 2)
+            data.push_back(visited_map[x][y + 1]);
+        
+        if(y - 1 > 0 and visited_map[x+1][y] != 0 and visited_map[x+1][y] != 2)
+            data.push_back(visited_map[x][y - 1]);
+        
+        if(y + 1 < column and x + 1 < row and visited_map[x+1][y] != 0 and visited_map[x+1][y] != 2)
+            data.push_back(visited_map[x + 1][y + 1]);
+        
+        if(x - 1 > 0 and y - 1 > 0 and visited_map[x+1][y] != 0 and visited_map[x+1][y] != 2)
+            data.push_back(visited_map[x - 1][y - 1]);
+
+        if(x - 1 > 0 and y + 1 < column and visited_map[x+1][y] != 0 and visited_map[x+1][y] != 2)
+           data.push_back(visited_map[x - 1][y + 1]);
+
+        if(x + 1 < row and y - 1 > 0 and visited_map[x+1][y] != 0 and visited_map[x+1][y] != 2)
+            data.push_back(visited_map[x + 1][y - 1]);
+
+
+
+    } while (visited_map[space_ship._get_Location()._X][space_ship._get_Location()._Y] != 5);
+    
 }
